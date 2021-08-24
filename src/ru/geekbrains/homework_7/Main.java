dd15d5be0ab4d5024827ce540d2bccc456cbd464
lekoal <coreglus@gmail.com>
2021-08-24 22:13:35 +0300

    Adding food in plate

--- src/ru/geekbrains/homework_7/Main.java
+++ src/ru/geekbrains/homework_7/Main.java
@@ -1,10 +1,13 @@
 package ru.geekbrains.homework_7;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Plate plate = new Plate(40);
         plate.info();
+        Scanner scan = new Scanner(System.in);
         Cat[] catArr = new Cat[6];
         catArr[0] = new Cat("Barsik", 5);
         catArr[1] = new Cat("Pushok", 8);
@@ -18,7 +21,17 @@ public class Main {
             plate.info();
             catArr[i].info();
         }
+        System.out.println("Do you wan to add food in plate? 1 - yes, 2 - no");
+        int choice = scan.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Choose quantity of food");
+                int foodQuantity = scan.nextInt();
+                plate.increaseFood(foodQuantity); // Добавление еды в тарелку
+                plate.info();
+            case 2: break;
+        }
 
-//        plate.increaseFood(10); // Добавление еды в тарелку
+
     }
 }
